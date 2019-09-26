cd01a20e6cae7cd664720a0b2009af3a1efe4989
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-09-26 20:04:01 +0200

    Add Reload Translations command

---
+++ Unifiedban.Terminal/Bot/Command/ReloadTranslations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Unifiedban.Models.Translation;
+
+namespace Unifiedban.Terminal.Bot.Command
+{
+    public class ReloadTranslations : ICommand
+    {
+        public void Execute(Message message)
+        {
+            if (CacheData.Operators
+                .SingleOrDefault(x => x.TelegramUserId == message.From.Id
+                && x.Level == Models.Operator.Levels.Super) == null)
+            {
+                MessageQueueManager.EnqueueMessage(
+                   new ChatMessage()
+                   {
+                       Timestamp = DateTime.UtcNow,
+                       Chat = message.Chat,
+                       ReplyToMessageId = message.MessageId,
+                       Text = CacheData.GetTranslation("en", "error_not_auth_command")
+                   });
+                Manager.BotClient.SendTextMessageAsync(
+                    chatId: Convert.ToInt64(CacheData.SysConfigs
+                            .Single(x => x.SysConfigId == "ControlChatId")
+                            .Value),
+                    parseMode: ParseMode.Markdown,
+                    text: String.Format(
+                        "User *{0}:{1}* tried to use command AddTranslation.",
+                        message.From.Id,
+                        message.From.Username)
+                );
+                return;
+            }
+
+            Program.InitializeTranslations();
+            MessageQueueManager.EnqueueMessage(
+                   new ChatMessage()
+                   {
+                       Timestamp = DateTime.UtcNow,
+                       Chat = message.Chat,
+                       ReplyToMessageId = message.MessageId,
+                       Text = CacheData.GetTranslation("en", "done", true)
+                   });
+        }
+
+        public void Execute(CallbackQuery callbackQuery) { }
+    }
+}
