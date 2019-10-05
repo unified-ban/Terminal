80f0c2fb5dc7d3cb2dbfc5aeaf1531db7af7e2fe
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-10-05 11:24:00 +0200

    New Utility: check if user is group admin

---
+++ Unifiedban.Terminal/Utils/ChatTools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unifiedban.Terminal.Utils
+{
+    public class ChatTools
+    {
+        public static bool IsUserAdmin(long chatId, long userId)
+        {
+            var administrators = Bot.Manager.BotClient.GetChatAdministratorsAsync(chatId).Result;
+            foreach(Telegram.Bot.Types.ChatMember member in administrators)
+            {
+                if (member.User.Id == userId)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
