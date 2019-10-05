e05739e55d9778a5c16219e2db258d687a6e1a9a
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-10-05 11:23:43 +0200

    New Utility: check if user is operator and, optionally, if has level

---
+++ Unifiedban.Terminal/Utils/BotTools.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unifiedban.Terminal.Bot;
+
+namespace Unifiedban.Terminal.Utils
+{
+    public class BotTools
+    {
+        public static bool IsUserOperator(long userId)
+        {
+            return CacheData.Operators
+                .SingleOrDefault(x => x.TelegramUserId == userId) != null ? true : false;
+        }
+
+        public static bool IsUserOperator(long userId, Models.Operator.Levels level)
+        {
+            return CacheData.Operators
+                .SingleOrDefault(x => x.TelegramUserId == userId &&
+                x.Level == level) != null ? true : false;
+        }
+    }
+}
