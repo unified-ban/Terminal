7c86a27727531419ac87852bdc05c23d76179b29
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2019-10-05 18:42:44 +0200

    (CLOSE) Task #22 - [Command] - Announce

--- Unifiedban.Terminal/Bot/Command/Announce.cs
+++ Unifiedban.Terminal/Bot/Command/Announce.cs
@@ -28,7 +28,7 @@ namespace Unifiedban.Terminal.Bot.Command
                             .Value),
                     parseMode: ParseMode.Markdown,
                     text: String.Format(
-                        "User *{0}:{1}* tried to use command Check.",
+                        "User *{0}:{1}* tried to use command Announce.",
                         message.From.Id,
                         message.From.Username)
                 );
@@ -43,15 +43,7 @@ namespace Unifiedban.Terminal.Bot.Command
                     return;
                 command = command.Split("@")[0];
             }
-            message.Text = message.Text.Remove(0, command.Length);
-
-            CommandMessage commandMessage = new CommandMessage()
-            {
-                Command = "PinAnnounce",
-                Message = message,
-                Timestamp = DateTime.UtcNow
-            };
-            CommandQueueManager.EnqueueMessage(commandMessage);
+            message.Text = message.Text.Remove(0, command.Length + 2);
 
             string messageHeader = CacheData.GetTranslation("en", "command_announce_header");
             string parsedMessage = messageHeader + "\n" + message.Text;
@@ -61,17 +53,11 @@ namespace Unifiedban.Terminal.Bot.Command
                     Timestamp = DateTime.UtcNow,
                     Chat = message.Chat,
                     ParseMode = ParseMode.Html,
-                    Text = parsedMessage
+                    Text = parsedMessage,
+                    PostSentAction = ChatMessage.PostSentActions.Pin
                 });
         }
 
         public void Execute(CallbackQuery callbackQuery) { }
-
-        public static void PinAnnounce(Message message)
-        {
-            int charsToRemove = message.Text.Split(']')[0].Length;
-            Manager.BotClient.EditMessageTextAsync(message.Chat.Id, message.MessageId, message.Text.Remove(0, charsToRemove));
-            Manager.BotClient.PinChatMessageAsync(message.Chat.Id, message.MessageId);
-        }
     }
 }
