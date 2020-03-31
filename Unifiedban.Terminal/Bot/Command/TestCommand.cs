d513b4503a8c7b8fb262888ebc05a7c6be128d1b
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-04-01 01:44:42 +0200

    Preliminary tests for rm command

--- Unifiedban.Terminal/Bot/Command/TestCommand.cs
+++ Unifiedban.Terminal/Bot/Command/TestCommand.cs
@@ -22,11 +22,22 @@ namespace Unifiedban.Terminal.Bot.Command
                 return;
             }
 
+            string[] data = message.Text.Split(" ");
+            if (data.Length >= 2)
+            {
+                handleRequest(data, message);
+                return;
+            }
+
             List<InlineKeyboardButton> confirmationButton = new List<InlineKeyboardButton>();
             confirmationButton.Add(InlineKeyboardButton.WithUrl("Start with command", "http://t.me/LinuxPixelHubBot?start=motd"));
             confirmationButton.Add(InlineKeyboardButton.WithCallbackData(
                                         CacheData.GetTranslation("en", "captcha_iamhuman", true),
-                                        $"/test " + message.From.Id
+                                        $"/test1 " + message.From.Id
+                                        ));
+            confirmationButton.Add(InlineKeyboardButton.WithCallbackData(
+                                        "RM",
+                                        $"/test1 rm"
                                         ));
 
             MessageQueueManager.EnqueueMessage(
@@ -40,14 +51,6 @@ namespace Unifiedban.Terminal.Bot.Command
                             confirmationButton
                         )
                     });
-
-            MessageQueueManager.EnqueueMessage(
-                    new ChatMessage()
-                    {
-                        Timestamp = DateTime.UtcNow,
-                        Chat = message.Chat,
-                        Text = Newtonsoft.Json.JsonConvert.SerializeObject(confirmationButton)
-                    });
         }
 
         public void Execute(CallbackQuery callbackQuery)
@@ -55,6 +58,39 @@ namespace Unifiedban.Terminal.Bot.Command
             if (!Utils.BotTools.IsUserOperator(callbackQuery.From.Id, Models.Operator.Levels.Super))
             {
                 return;
+            }
+
+            string[] data = callbackQuery.Data.Split(" ");
+            if (data.Length < 2)
+                return;
+
+            handleRequest(data, callbackQuery.Message);
+        }
+
+        private void handleRequest(string[] data, Message message)
+        {
+            switch (data[1])
+            {
+                case "rm":
+                    if (data.Length == 3)
+                        deleteLastMessages(message, Convert.ToInt32(data[2]));
+                    else
+                        deleteLastMessages(message);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void deleteLastMessages(Message message, int amount = 1)
+        {
+            int startMessage = message.ReplyToMessage != null ? message.ReplyToMessage.MessageId : message.MessageId - 1;
+            int nextMessage = message.ReplyToMessage != null ? message.ReplyToMessage.MessageId : message.MessageId - 1;
+
+            while (nextMessage >= startMessage - amount)
+            {
+                Manager.BotClient.DeleteMessageAsync(message.Chat.Id, nextMessage);
+                nextMessage--;
             }
         }
     }
