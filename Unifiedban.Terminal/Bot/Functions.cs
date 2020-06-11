16ab90cf8b0d21342245fee71d16ebedeb6332cb
Pietro di Caprio <pietro.dicaprio@subpixel.it>
2020-06-11 21:18:11 +0200

    ignore bots that join group

--- Unifiedban.Terminal/Bot/Functions.cs
+++ Unifiedban.Terminal/Bot/Functions.cs
@@ -1,4 +1,4 @@
-﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
@@ -170,7 +170,8 @@ namespace Unifiedban.Terminal.Bot
             foreach (User member in message.NewChatMembers)
             {
                 if (member.Id == Manager.MyId ||
-                    member.Id == 777000) // Telegram's official updateServiceNotification
+                    member.Id == 777000 || // Telegram's official updateServiceNotification
+                    member.IsBot) 
                 {
                     continue;
                 }
